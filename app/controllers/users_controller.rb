4a8af4f70a83a9eda491b28938b62e83185a18e8
Gerald Bivens <geraldbivens@gmail.com>
2021-01-06 17:26:15 -0700

    Add create action to create a user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+    def create
+        @user = User.create(
+            username: params[:username],
+            password: params[:password]
+        )
+
+        render json: @user, status: :created
+    end
 end
