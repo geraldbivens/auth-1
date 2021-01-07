4f78270afaf94cef6c01affeda5ee7857e73839e
Gerald Bivens <geraldbivens@gmail.com>
2021-01-07 11:35:56 -0700

    Add index and show actions and routes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
+    
+    def index
+        @users = User.all
+
+        render json: @users
+    end
+
+    def show
+        @user = User.find(params[:find])
+
+        render json: @user
+    end
+    
     def create
         @user = User.create(
             username: params[:username],
